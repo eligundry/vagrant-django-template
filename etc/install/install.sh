987b83308440783ce84e938d0abe434eee1163e5
Eli Gundry <eligundry@gmail.com>
2015-03-01 08:42:05 -0500

    Removes npm from provisioning

--- etc/install/install.sh
+++ etc/install/install.sh
@@ -55,19 +55,6 @@ fi
 # bash environment global setup
 cp -p $PROJECT_DIR/etc/install/bashrc /home/vagrant/.bashrc
 
-# Node.js, CoffeeScript and LESS
-if ! command -v npm; then
-    wget http://nodejs.org/dist/v0.10.0/node-v0.10.0.tar.gz
-    tar xzf node-v0.10.0.tar.gz
-    cd node-v0.10.0/
-    ./configure && make && make install
-    cd ..
-    rm -rf node-v0.10.0/ node-v0.10.0.tar.gz
-fi
-if ! command -v coffee; then
-    npm install -g coffee-script
-fi
-
 # ---
 
 # postgresql setup for project
